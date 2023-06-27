5e8ad2fd7adda032f50339ce6be3e909e386f657
jvelaz824 <jvelaz824@gmail.com>
2023-06-27 12:19:20 -0400

    Add Accounting Software Welcome Page

---
+++ AccountingSoftwareWelcome.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Welcome to Acme Account Systems \n");
+        Console.Write("Remember, we're \"accounting\" on you!");
+        Console.Read();
+    }
+}
