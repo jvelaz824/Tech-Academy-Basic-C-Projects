219a79992306c70171a367bff4db52ac7aa5d5a9
jvelaz824 <jvelaz824@gmail.com>
2023-07-07 18:50:01 -0400

    Editing class

--- Method Class Assignment/MoreMath.cs
+++ Method Class Assignment/MoreMath.cs
@@ -11,7 +11,7 @@ namespace Method_Class_Assignment
         public static void Math1(int num1, int num2)
         {
             int total = num1 + 50;
-            Console.WriteLine("The first number passed plus 50 is: " + total);
+            Console.WriteLine("The first number passed ({0}) plus 50 is: {1}", num1, total);
             Console.WriteLine("The second number passed is: " + num2);
         }
     }
