dc2ec4ba1c1c4015affde8cd3aaa761c0fd83905
jvelaz824 <jvelaz824@gmail.com>
2023-07-07 14:48:30 -0400

    Adding Calling Methods Assignment to repository

---
+++ Calling Methods Assignment/MathOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    public class MathOperations
+    {
+        
+        //Declare method that takes an integer input and will return an int
+        public int MultiplyBy5 (int x)
+        {
+            int total = x * 5; //multiply the input by 5 and assign the result to an int variable named total
+            return total; //return total
+        }
+
+        //Declare a method that takes an integer input and will return an int
+        //This method is constructed slightly different than above for practice sake
+        public int Add50(int x)
+        {
+            return x + 50; //return the input with 50 added to it
+            
+        }
+
+        //Declare a method that takes an integer input. but will return a string
+        public string OddOrEven(int x)
+        {
+            int total = x % 2; //int variable will hold the result of x % 2
+            string result; //declare a string variable, which will be used for the return
+            if (total == 0) //if x % 2 is equal to 0, it is even, so assign a string saying so to 'result'
+            {
+                result = ("The remainder of " + x + " divided by 2 is: " + total + ", making it even.");
+            }
+            else //Else, it is odd, so assign a string saying so to 'result'
+            {
+                result = ("The remainder of " + x + " divided by 2 is: " + total + ", making it odd.");
+            }
+
+            return result; //return the result string
+        }
+    }
+}
---
+++ Calling Methods Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations results = new MathOperations(); //instiantiates an object of the Math_operations class
+
+            //Ask user to input a number, converts it to an int, and assigns it to an int variable
+            Console.WriteLine("We are going to perform some math operations. \nPlease enter a whole number to test with.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+
+            //calls Add50 method and outputs the result
+            Console.WriteLine(num + " + 50 equals: " + results.Add50(num));
+
+            //calls MultiplyBy5 method and outputs the result
+            Console.WriteLine(num + " multiplied by 5 equals: " + results.MultiplyBy5(num));
+
+            //calls OddOrEven method and outputs the result
+            Console.WriteLine(results.OddOrEven(num));
+
+            Console.Read();
+        }
+
+    }
+}
