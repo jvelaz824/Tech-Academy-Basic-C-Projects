7430d2619828cc5942e03d72e217b30e67d39b21
jvelaz824 <jvelaz824@gmail.com>
2023-07-07 18:49:19 -0400

    Adding Method Class Assignment to repository

---
+++ Method Class Assignment/MoreMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+   public class MoreMath
+    {
+        public static void Math1(int num1, int num2)
+        {
+            int total = num1 + 50;
+            Console.WriteLine("The first number passed plus 50 is: " + total);
+            Console.WriteLine("The second number passed is: " + num2);
+        }
+    }
+}
---
+++ Method Class Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The math fun continues!"); //displays a start message to the user 
+
+            MoreMath obj1 = new MoreMath(); //instantiates the MoreMathClass
+
+            MoreMath.Math1(78, 5); //Calls the 'Math1' method and passes two integers as arguments
+
+            int x = 15; //creating a intger variable with the value '15'
+            int y = 82; //creating an integer variable with the value '82'
+
+            MoreMath.Math1(num1: x, num2: y); //calling the 'Math1' method, specifyng the paramters by name, passing in the two int variables above
+
+            Console.Read();
+
+
+        }
+    }
+}
