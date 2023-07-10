6c45ede398af66f7941368dc8ebe75a390da9118
jvelaz824 <jvelaz824@gmail.com>
2023-07-10 12:38:14 -0400

    Adding Methods and Objects Assignment to repository

---
+++ Methods and Objects Assignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    public class Person
+    {
+        public string FirstName { get; set; } // a string property that holds a person's First Name
+        public string LastName { get; set; }// a string propert that holds a person's Last Name
+
+        //the following is a void method that outputs the first and last name of a 'Person' object
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+
+    }
+}
---
+++ Methods and Objects Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiates and initalizs an 'Employee' object 
+            Employee test = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            test.SayName(); //Calls the super class method 'SayName' method on the 'Employee' object
+
+            Console.Read();
+
+        }
+    }
+}
