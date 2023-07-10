22b8db5d6e74ffa6b1514fe9e7bae9da12007912
jvelaz824 <jvelaz824@gmail.com>
2023-07-10 13:50:01 -0400

    Adding Abstract Class Assignment to repository

---
+++ Abstract Class Assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    class Employee : Person //Inherits from the 'Person' class
+    {
+        public override void SayName() //Implements the 'SayName' method from the 'Person' class 
+        {
+            Console.WriteLine("Employee Information: "); //outputs a header line
+            base.SayName();
+        }
+    }
+}
---
+++ Abstract Class Assignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    public abstract class Person //sets the 'Person' class as abstract
+    {
+        public string firstName { get; set; } //Defines a string property 'firstName'
+        public string lastName { get; set; } //Defines a string property 'lastName
+
+        //the following creates a virtual method called 'SayName', which prints the first and last name of an object
+        public virtual void SayName()
+        {
+            Console.WriteLine("Full Name: {0} {1}", firstName, lastName);
+        }
+
+    }
+}
