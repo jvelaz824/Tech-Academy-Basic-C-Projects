f985d336f8dcee6cb2bce11e6d26a60ee274b4c9
jvelaz824 <jvelaz824@gmail.com>
2023-07-11 18:14:10 -0400

    Adding Struct Assignment to repository

---
+++ Struct Assignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    struct Number //Creates a struct called 'Number'
+    {
+        //Creates a decimal property named 'Amount'
+        public decimal Amount { get; set; } 
+    }
+}
---
+++ Struct Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiates an object of data type Number and sets Amount to 250
+            Number num = new Number() { Amount = 250 };
+            
+            Console.WriteLine(num.Amount); //Displays Amount to user
+
+            Console.Read();
+
+
+        }
+    }
+}
