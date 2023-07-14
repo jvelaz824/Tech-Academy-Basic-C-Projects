8e62a08985ae564be5fd303a7652513d6d77dcc6
jvelaz824 <jvelaz824@gmail.com>
2023-07-14 13:16:40 -0400

    Adding Constructor Assignment to repository

---
+++ Constructor Assignment/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    public class Circle
+    {
+        //Declares two class constructors and chains them together
+        public Circle(int Radius, int Diameter)
+        {
+
+            diameter = Diameter;
+            radius = Radius;
+        }
+
+        public Circle(int Radius) : this(Radius, 100)
+        {
+        }
+
+
+        public int diameter { get; set; } //Declares a public integer property
+        public int radius { get; set; } //Declares a public integer property
+
+    
+            
+
+    }
+}
---
+++ Constructor Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates a new ibject of the 'Circle' class
+            Circle newCircle = new Circle(55);
+
+            //Declares a const double variable that holds the value of pi 
+            const double pi = 3.14;
+
+            //Declares a variable using the "var" keyword and assigns the formula for the circumference of a circle to its value
+            var circumference = 2 * pi * newCircle.radius;
+
+            //Outputs a starting message to the user
+            Console.WriteLine("We created a circle with a radius of {0}. \nLet's calculate its circumference.", newCircle.radius);
+       
+
+            //Outputs the circumference of the circle
+            Console.WriteLine("The circumference of the circle is: {0}", circumference);
+
+            Console.Read();
+        }
+    }
+}
