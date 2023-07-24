98afa5b28c61f7fd776d2e2df1d5cf1978734655
jvelaz824 <jvelaz824@gmail.com>
2023-07-24 11:15:09 -0400

    Adding Final Challenge Assignment to repository

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                Console.WriteLine("Welcome to The Tech Academy. \n");
+
+                var student = new Student();
+
+                Console.WriteLine("Please enter a student's name.");
+                student.StudentName = Console.ReadLine();
+
+                Console.WriteLine("What is their date of birth? Please enter as mm/dd/yyy.");
+                student.DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                Console.WriteLine("Student added to the database. \nThank you! \n");
+
+                Console.WriteLine("Please enter any key to exit.");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ FinalChallenge/FinalChallenge/SchoolContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace FinalChallenge
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
